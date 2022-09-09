14d2887f4bd199935c5719b538d2d36dff328797
Arash <97286379+Ghalbeyou@users.noreply.github.com>
2022-09-09 12:30:19 +0430

    Advanced config
    New config for this project

--- fuckdevtools.js
+++ fuckdevtools.js
@@ -5,22 +5,32 @@ https://github.com/GhalbeYou
 ---------------------------------------
 This is a simple script that will disable the developer tools in the browser.
 */
-
+var disable_right_click = true;
+var disable_f12 = true;
+var disable_csi = true;
 document.onkeydown = function(event) {
-    if (event.keyCode == 123) {
-        event.preventDefault();
-        alert("Devtools?");
-        location.replace("about:blank");
-    
+    if (disable_f12 == true){
+        if (event.keyCode == 123) {
+            event.preventDefault();
+    //        alert("Devtools?");
+            location.replace("about:blank");
+
+        }
     }
     // if the user pressed the Control + Shift + I key
-    else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-        event.preventDefault();
-        alert("Devtools?");
-        location.replace("about:blank");
+    if (disable_csi == true){
+        
+        if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+            event.preventDefault();
+    //        alert("Devtools?");
+            location.replace("about:blank");
+        }
     }
 }
-document.oncontextmenu = function() {
-    event.preventDefault();
-    alert("Devtools?");
+if (disable_right_click == true){
+    
+    document.oncontextmenu = function() {
+        event.preventDefault();
+        alert("Devtools?");
+    }
 }
