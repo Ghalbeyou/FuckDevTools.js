57a3de4ff85d862c8bb4de69f142f78505c77488
Arash <97286379+Ghalbeyou@users.noreply.github.com>
2023-09-26 17:02:54 +0330

    Removed Alert & Location Href

--- fuckdevtools.js
+++ fuckdevtools.js
@@ -14,8 +14,6 @@ document.onkeydown = function(event) {
     if (disable_f12 == true){
         if (event.keyCode == 123) {
             event.preventDefault();
-            location.replace("about:blank");
-
         }
     }
     // if the user pressed the Control + Shift + I key
@@ -23,7 +21,6 @@ document.onkeydown = function(event) {
         
         if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
             event.preventDefault();
-            location.replace("about:blank");
         }
     }
 }
@@ -31,6 +28,5 @@ if (disable_right_click == true){
     
     document.oncontextmenu = function() {
         event.preventDefault();
-        alert("Devtools?");
     }
 }
